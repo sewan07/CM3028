d27c7933533a0d222aa36d7b9685993728d55a2c
Sewan <kido184@hotmail.com>
2016-12-04 14:51:25 +0000

    Index File

--- public/site_admin.php
+++ public/site_admin.php
@@ -1,38 +1,30 @@
-<?php require_once("../includes/db_connection.php"); ?>
-<?php require_once("../includes/functions.php"); ?>
-<?php
-// 2. Perform database query
-$query  = "SELECT * ";
-$query .= "FROM subjects ";
-$query .= "WHERE visible = 1 ";
-$query .= "ORDER BY position ASC";
-$result = mysqli_query($connection, $query);
-confirm_query($result);
-?>
-<?php include("../includes/layouts/header.php"); ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+    "http://www.w3.org/TR/html4/loose.dtd">
 
+<html lang="en">
+<head>
+    <title>untitled</title>
+    <link href="stylesheets/public.css" media="all" rel="stylesheet" type="text/css" />
+</head>
+<body>
+<div id="header">
+    <h1>Portlethen</h1>
+</div>
 <div id="main">
     <div id="navigation">
-        <ul class="subjects">
-            <?php
-            // 3. Use returned data (if any)
-            while($subject = mysqli_fetch_assoc($result)) {
-                // output data from each row
-                ?>
-                <li><?php echo $subject["menu_name"] . " (" . $subject["id"] . ")"; ?></li>
-                <?php
-            }
-            ?>
-        </ul>
+        &nbsp;
     </div>
     <div id="page">
-        <h2>Manage Content</h2>
-
+        <h2>Admin Menu</h2>
+        <p>Welcome to the Site Administration area.</p>
+        <ul>
+            <li><a href="manage_content.php">Manage Website Content</a></li>
+            <li><a href="manage_admins.php">Manage Admin Users</a></li>
+            <li><a href="logout.php">Logout</a></li>
+        </ul>
     </div>
 </div>
-<?php
-// 4. Release returned data
-mysqli_free_result($result);
-?>
+<div id="footer">Copyright 20xx, Widget Corp</div>
 
-<?php include("../includes/layouts/footer.php"); ?>
+</body>
+</html>
