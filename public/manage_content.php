01c347982cbdd6970db0b7eddc895b0da893e3e4
Sewan <kido184@hotmail.com>
2016-12-04 13:57:08 +0000

    Index File

--- public/manage_content.php
+++ public/manage_content.php
@@ -1,5 +1,5 @@
 <?php require_once("../includes/db_connection.php"); ?>
-<?php require_once("../includes/functions.php"); ?>
+
 <?php include("../includes/layouts/header.php"); ?>
 
     <div id="main">
