f95ab8fe6bf922a9ba84430f5f9818c2f5f6fab4
Sewan <kido184@hotmail.com>
2016-12-04 14:52:39 +0000

    Index File

--- public/site_admin.php
+++ public/site_admin.php
@@ -4,7 +4,7 @@
 <html lang="en">
 <head>
     <title>untitled</title>
-    <link href="stylesheets/public.css" media="all" rel="stylesheet" type="text/css" />
+    <link href="css/public.css" media="all" rel="stylesheet" type="text/css" />
 </head>
 <body>
 <div id="header">
