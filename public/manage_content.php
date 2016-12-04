de257cc99aa94ce1eadd0aa5c616124e1b821e1b
Sewan <kido184@hotmail.com>
2016-12-04 14:38:39 +0000

    Index File

--- public/manage_content.php
+++ public/manage_content.php
@@ -1,5 +1,18 @@
-<?php require_once("../includes/db_connection.php"); ?>
-<?php require_once("../includes/functions.php"); ?>
+<?php
+// 1. Create a database connection
+$dbhost = "localhost";
+$dbuser = "widget_cms";
+$dbpass = "secretpassword";
+$dbname = "widget_corp";
+$connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+// Test if connection succeeded
+if(mysqli_connect_errno()) {
+    die("Database connection failed: " .
+        mysqli_connect_error() .
+        " (" . mysqli_connect_errno() . ")"
+    );
+}
+?>
 <?php
 // 2. Perform database query
 $query  = "SELECT * ";
@@ -7,32 +20,41 @@ $query .= "FROM subjects ";
 $query .= "WHERE visible = 1 ";
 $query .= "ORDER BY position ASC";
 $result = mysqli_query($connection, $query);
-confirm_query($result);
+// Test if there was a query error
+if (!$result) {
+    die("Database query failed.");
+}
 ?>
-<?php include("../includes/layouts/header.php"); ?>
 
-<div id="main">
-    <div id="navigation">
-        <ul class="subjects">
-            <?php
-            // 3. Use returned data (if any)
-            while($subject = mysqli_fetch_assoc($result)) {
-                // output data from each row
-                ?>
-                <li><?php echo $subject["menu_name"] . " (" . $subject["id"] . ")"; ?></li>
-                <?php
-            }
+    <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+        "http://www.w3.org/TR/html4/loose.dtd">
+
+    <html lang="en">
+    <head>
+        <title>Databases</title>
+    </head>
+    <body>
+
+    <ul>
+        <?php
+        // 3. Use returned data (if any)
+        while($subject = mysqli_fetch_assoc($result)) {
+            // output data from each row
             ?>
-        </ul>
-    </div>
-    <div id="page">
-        <h2>Manage Content</h2>
+            <li><?php echo $subject["menu_name"] . " (" . $subject["id"] . ")"; ?></li>
+            <?php
+        }
+        ?>
+    </ul>
 
-    </div>
-</div>
+    <?php
+    // 4. Release returned data
+    mysqli_free_result($result);
+    ?>
+    </body>
+    </html>
+
 <?php
-// 4. Release returned data
-mysqli_free_result($result);
+// 5. Close database connection
+mysqli_close($connection);
 ?>
-
-<?php include("../includes/layouts/footer.php"); ?>
