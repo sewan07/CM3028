b185fdd97b347586a8885a9b7a00b7b376703f2c
Sewan <kido184@hotmail.com>
2016-12-05 10:01:09 +0000

    Index File

--- public/admin.php
+++ public/admin.php
@@ -2,21 +2,27 @@
 
 <?php require_once("../includes/functions.php"); ?>
 
-<?php include("../includes/layouts/header.php"); ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+    "http://www.w3.org/TR/html4/loose.dtd">
 
-<div id="main">
-  <div id="navigation">
-    &nbsp;
-  </div>
-  <div id="page">
-    <h2>Admin Menu</h2>
-    <p>Welcome to admin area.</p>
-    <ul>
-      <li><a href="manage_content.php">Manage Website Content</a></li>
-      <li><a href="manage_admins.php">Manage Admin Users</a></li>
-      <li><a href="logout.php">Logout</a></li>
-    </ul>
-  </div>
+<html lang="en">
+<head>
+  <title> Portlethen </title>
+  <link href="stylesheets/public.css" media="all" rel="stylesheet" type="text/css" />
+</head>
+<body>
+<div id="header">
+  <h1>Go Portlethen</h1>
 </div>
+
+  <div id="navigation">
+    <nav>
+      <a href="manage_content" class="navHome">Manage Website Content</a>
+      <a href="manage_admins" class="navAbout">Manage Admin Users</a>
+      <a href="logout.php" class="navPort">Logout</a>
+    </nav>
+  </div>
+  </div>
+</body>
 
 <?php include("../includes/layouts/footer.php"); ?>
