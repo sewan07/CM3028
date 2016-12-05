3eb6bb86f69f12f031579ae2f15e101e1dd660e5
Sewan <kido184@hotmail.com>
2016-12-05 09:44:21 +0000

    Index File

--- public/admin.php
+++ public/admin.php
@@ -10,7 +10,7 @@
   </div>
   <div id="page">
     <h2>Admin Menu</h2>
-    <p>Welcome to the admin area, <?php echo htmlentities($_SESSION["username"]); ?>.</p>
+    <p>Welcome to the admin area.</p>
     <ul>
       <li><a href="manage_content.php">Manage Website Content</a></li>
       <li><a href="manage_admins.php">Manage Admin Users</a></li>
