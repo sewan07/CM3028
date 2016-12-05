46e1d00decba682f276d16f2ec051030436a1248
Sewan <kido184@hotmail.com>
2016-12-05 09:52:21 +0000

    Index File

--- public/admin.php
+++ public/admin.php
@@ -10,7 +10,7 @@
   </div>
   <div id="page">
     <h2>Admin Menu</h2>
-    <p>Welcome to the admin area.</p>
+    <p>Welcome to admin area.</p>
     <ul>
       <li><a href="manage_content.php">Manage Website Content</a></li>
       <li><a href="manage_admins.php">Manage Admin Users</a></li>
