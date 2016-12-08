552bc8df6290755cd7e004d0a74c56ae61cf86a1
Sewan <kido184@hotmail.com>
2016-12-08 23:20:01 +0000

    Index File

--- includes/layouts/header.php
+++ includes/layouts/header.php
@@ -8,10 +8,10 @@
 
 <html lang="en">
 	<head>
-		<title>Widget Corp <?php if ($layout_context == "admin") { echo "Admin"; } ?></title>
+		<title>Go Portlethen <?php if ($layout_context == "admin") { echo "Admin"; } ?></title>
 		<link href="stylesheets/public.css" media="all" rel="stylesheet" type="text/css" />
 	</head>
 	<body>
     <div id="header">
-      <h1>Widget Corp <?php if ($layout_context == "admin") { echo "Admin"; } ?></h1>
+      <h1>Go Portlethen <?php if ($layout_context == "admin") { echo "Admin"; } ?></h1>
     </div>
