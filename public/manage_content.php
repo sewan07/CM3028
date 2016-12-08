5feb243d22331c9f8c7befd1f2c12c2618d746f7
Sewan <kido184@hotmail.com>
2016-12-08 07:45:18 +0000

    Index File

--- public/manage_content.php
+++ public/manage_content.php
@@ -1,53 +1,54 @@
 <?php require_once("../includes/db_connection.php"); ?>
 <?php require_once("../includes/functions.php"); ?>
-
 <?php include("../includes/layouts/header.php"); ?>
 
+<?php
+if (isset($_GET["subject"])) {
+	$selected_subject_id = $_GET["subject"];
+	$selected_page_id = null;
+} elseif (isset($_GET["page"])) {
+	$selected_subject_id = null;
+	$selected_page_id = $_GET["page"];
+} else {
+	$selected_subject_id = null;
+	$selected_page_id = null;
+}
+
+?>
 <div id="main">
-  <div id="navigation">
+	<div id="navigation">
 		<ul class="subjects">
+			<?php $subject_set = find_all_subjects(); ?>
 			<?php
-              $subject_set = find_all_subjects();
-			?>
-		<?php
-			// 3. Use returned data (if any)
 			while($subject = mysqli_fetch_assoc($subject_set)) {
-				// output data from each row
-		?>
+				?>
 				<li>
 					<a href="manage_content.php?subject=<?php echo urlencode($subject["id"]); ?>"><?php echo $subject["menu_name"]; ?></a>
 					<?php $page_set = find_pages_for_subject($subject["id"]); ?>
 					<ul class="pages">
 						<?php
-						// 3. Use returned data (if any)
 						while($page = mysqli_fetch_assoc($page_set)) {
-						// output data from each row
+							?>
+							<li>
+								<a href="manage_content.php?page=<?php echo urlencode($page["id"]); ?>"><?php echo $page["menu_name"]; ?></a>
+							</li>
+							<?php
+						}
 						?>
-						<li>
-							<a href="manage_content.php?page=<?php echo urlencode($page["id"]); ?>"><?php echo $page["menu_name"]; ?></a>
-						</li>
-						<?php
-						// 4. Release returned data
-						mysqli_free_result($page_set);
-						?>
-
+						<?php mysqli_free_result($page_set); ?>
 					</ul>
 				</li>
-		<?php
+				<?php
 			}
-		?>
-
-			<?php
-			// 4. Release returned data
-			mysqli_free_result($subject_set);
 			?>
+			<?php mysqli_free_result($subject_set); ?>
 		</ul>
-  </div>
-  <div id="page">
-    <h2>Manage Content</h2>
-
-  </div>
+	</div>
+	<div id="page">
+		<h2>Manage Content</h2>
+		<?php echo $selected_subject_id; ?><br />
+		<?php echo $selected_page_id; ?>
+	</div>
 </div>
-
 
 <?php include("../includes/layouts/footer.php"); ?>
