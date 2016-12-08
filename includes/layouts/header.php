57d90692165044f8bda1ad3ab6b54246454cecee
Sewan <kido184@hotmail.com>
2016-12-08 06:24:53 +0000

    Index File

--- includes/layouts/header.php
+++ includes/layouts/header.php
@@ -1,13 +1,12 @@
-
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
    "http://www.w3.org/TR/html4/loose.dtd">
 
 <html lang="en">
 	<head>
-		<title>Portlethen </title>
+		<title>Widget Corp</title>
 		<link href="stylesheets/public.css" media="all" rel="stylesheet" type="text/css" />
 	</head>
 	<body>
     <div id="header">
-      <h1>Portlethen </h1>
+      <h1>Widget Corp</h1>
     </div>
