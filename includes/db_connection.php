27434a6b175a18d5fc31a9a62e662004dea5d927
Sewan <kido184@hotmail.com>
2016-12-08 08:56:17 +0000

    Index File

--- includes/db_connection.php
+++ includes/db_connection.php
@@ -1,10 +1,11 @@
 <?php
+	define("DB_SERVER", "us-cdbr-azure-southcentral-f.cloudapp.net");
+	define("DB_USER", "b762b602ac9c7b");
+	define("DB_PASS", "360b7be5");
+	define("DB_NAME", "cm3028rgu");
+
   // 1. Create a database connection
-  $dbhost = "us-cdbr-azure-southcentral-f.cloudapp.net";
-  $dbuser = "b762b602ac9c7b";
-  $dbpass = "360b7be5";
-  $dbname = "cm3028rgu";
-  $connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+  $connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
   // Test if connection succeeded
   if(mysqli_connect_errno()) {
     die("Database connection failed: " . 
