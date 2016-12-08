b9372f48591dc0377ce33036eaaded10895deae4
Sewan <kido184@hotmail.com>
2016-12-08 07:59:46 +0000

    Index File

--- public/manage_content.php
+++ public/manage_content.php
@@ -22,21 +22,33 @@ if (isset($_GET["subject"])) {
 			<?php
 			while($subject = mysqli_fetch_assoc($subject_set)) {
 				?>
-				<li class="selected">
-					<a href="manage_content.php?subject=<?php echo urlencode($subject["id"]); ?>"><?php echo $subject["menu_name"]; ?></a>
-					<?php $page_set = find_pages_for_subject($subject["id"]); ?>
-					<ul class="pages">
-						<?php
-						while($page = mysqli_fetch_assoc($page_set)) {
-							?>
-							<li>
-								<a href="manage_content.php?page=<?php echo urlencode($page["id"]); ?>"><?php echo $page["menu_name"]; ?></a>
-							</li>
-							<?php
-						}
+				<?php
+				echo "<li";
+				if ($subject["id"] == $selected_subject_id) {
+					echo " class=\"selected\"";
+				}
+				echo ">";
+				?>
+				<a href="manage_content.php?subject=<?php echo urlencode($subject["id"]); ?>"><?php echo $subject["menu_name"]; ?></a>
+				<?php $page_set = find_pages_for_subject($subject["id"]); ?>
+				<ul class="pages">
+					<?php
+					while($page = mysqli_fetch_assoc($page_set)) {
 						?>
-						<?php mysqli_free_result($page_set); ?>
-					</ul>
+						<?php
+						echo "<li";
+						if ($page["id"] == $selected_page_id) {
+							echo " class=\"selected\"";
+						}
+						echo ">";
+						?>
+						<a href="manage_content.php?page=<?php echo urlencode($page["id"]); ?>"><?php echo $page["menu_name"]; ?></a>
+						</li>
+						<?php
+					}
+					?>
+					<?php mysqli_free_result($page_set); ?>
+				</ul>
 				</li>
 				<?php
 			}
