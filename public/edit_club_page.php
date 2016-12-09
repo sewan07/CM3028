6fa6b56f4413bf2cf4249144bf50523712b3692c
Sewan <kido184@hotmail.com>
2016-12-09 19:51:28 +0000

    Index File

--- public/edit_club_page.php
+++ public/edit_club_page.php
@@ -6,61 +6,76 @@
 
 <?php find_selected_page(); ?>
 
+<?php
+// Unlike new_page.php, we don't need a subject_id to be sent
+// We already have it stored in pages.subject_id.
+if (!$current_page) {
+    // page ID was missing or invalid or
+    // page couldn't be found in database
+    redirect_to("manage_content.php");
+}
+?>
 
-<?php require_once("../includes/db_connection.php"); ?>
-<?php require_once("../includes/functions.php"); ?>
+<?php
+if (isset($_POST['submit'])) {
+    // Process the form
+
+    $id = $current_page["id"];
+    $menu_name = mysql_prep($_POST["menu_name"]);
+    $position = (int) $_POST["position"];
+    $visible = (int) $_POST["visible"];
+    $content = mysql_prep($_POST["content"]);
+
+    // validations
+    $required_fields = array("menu_name", "position", "visible", "content");
+    validate_presences($required_fields);
+
+    $fields_with_max_lengths = array("menu_name" => 30);
+    validate_max_lengths($fields_with_max_lengths);
+
+    if (empty($errors)) {
+
+        // Perform Update
+
+        $query  = "UPDATE pages SET ";
+        $query .= "menu_name = '{$menu_name}', ";
+        $query .= "position = {$position}, ";
+        $query .= "visible = {$visible}, ";
+        $query .= "content = '{$content}' ";
+        $query .= "WHERE id = {$id} ";
+        $query .= "LIMIT 1";
+        $result = mysqli_query($connection, $query);
+
+        if ($result && mysqli_affected_rows($connection) == 1) {
+            // Success
+            $_SESSION["message"] = "Page updated.";
+            redirect_to("manage_content.php?page={$id}");
+        } else {
+            // Failure
+            $_SESSION["message"] = "Page update failed.";
+        }
+
+    }
+} else {
+    // This is probably a GET request
+
+} // end: if (isset($_POST['submit']))
+
+?>
+
+<?php $layout_context = "admin"; ?>
 <?php include("../includes/layouts/header.php"); ?>
 
 <div id="main">
     <div id="navigation">
-        <ul class="subjects">
-            <?php
-            $query  = "SELECT * ";
-            $query .= "FROM subjects ";
-            $query .= "WHERE visible = 1 ";
-            $query .= "AND id = 2 ";
-            $query .= "ORDER BY position ASC";
-            $subject_set = mysqli_query($connection, $query);
-            confirm_query($subject_set);
-            ?>
-            <?php
-            while($subject = mysqli_fetch_assoc($subject_set)) {
-                ?>
-                <li>
-                    <?php echo $subject["menu_name"]; ?>
-                    <?php
-                    $query  = "SELECT * ";
-                    $query .= "FROM pages ";
-                    $query .= "WHERE visible = 1 ";
-                    $query .= "AND id = 1 ";
-                    $query .= "ORDER BY position ASC";
-                    $page_set = mysqli_query($connection, $query);
-                    confirm_query($page_set);
-                    ?>
-                    <ul class="pages">
-                        <?php
-                        while($page = mysqli_fetch_assoc($page_set)) {
-                            ?>
-                            <a href="edit_club_page.php"> <li><?php echo $page["menu_name"]; ?></li> </a>
-                            <?php
-                        }
-                        ?>
-                        <?php mysqli_free_result($page_set); ?>
-                    </ul>
-                </li>
-                <?php
-            }
-            ?>
-            <?php mysqli_free_result($subject_set); ?>
-        </ul>
+        <?php echo navigation($current_subject, $current_page); ?>
     </div>
-
     <div id="page">
         <?php echo message(); ?>
         <?php echo form_errors($errors); ?>
 
         <h2>Edit Page: <?php echo htmlentities($current_page["menu_name"]); ?></h2>
-        <form action="edit_club_page.php?page=<?php echo urlencode($current_page["id"]); ?>" method="post">
+        <form action="edit_page.php?page=<?php echo urlencode($current_page["id"]); ?>" method="post">
             <p>Menu name:
                 <input type="text" name="menu_name" value="<?php echo htmlentities($current_page["menu_name"]); ?>" />
             </p>
@@ -84,7 +99,7 @@
                 &nbsp;
                 <input type="radio" name="visible" value="1" <?php if ($current_page["visible"] == 1) { echo "checked"; } ?>/> Yes
             </p>
-            <p>Contents:<br />
+            <p>Content:<br />
                 <textarea name="content" rows="20" cols="80"><?php echo htmlentities($current_page["content"]); ?></textarea>
             </p>
             <input type="submit" name="submit" value="Edit Page" />
@@ -99,5 +114,3 @@
 </div>
 
 <?php include("../includes/layouts/footer.php"); ?>
-
-
