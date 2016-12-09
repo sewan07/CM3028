3f183993e3869ef4cb880200d70ebe676391e750
Sewan <kido184@hotmail.com>
2016-12-09 19:18:55 +0000

    Index File

--- includes/functions.php
+++ includes/functions.php
@@ -28,7 +28,7 @@
 		$query .= "FROM pages ";
 		$query .= "WHERE visible = 1 ";
 		$query .= "AND subject_id = {$safe_subject_id} ";
-		$query .= "AND id = 1 ";
+
 		$query .= "ORDER BY position ASC";
 		$page_set = mysqli_query($connection, $query);
 		confirm_query($page_set);
