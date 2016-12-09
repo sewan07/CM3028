d4a9a2d03d20b5303e1bd96fcbf9b95d2068e274
Sewan <kido184@hotmail.com>
2016-12-09 18:50:29 +0000

    Index File

--- public/manage_content.php
+++ public/manage_content.php
@@ -15,40 +15,15 @@ if (isset($_GET["subject"])) {
 }
 
 ?>
-<div id="main">
-	<div id="navigation">
-		<ul class="subjects">
-			<?php $subject_set = find_all_subjects(); ?>
-			<?php
-			while($subject = mysqli_fetch_assoc($subject_set)) {
-				?>
-				<li>
-					<a href="manage_content.php?subject=<?php echo urlencode($subject["id"]); ?>"><?php echo $subject["menu_name"]; ?></a>
-					<?php $page_set = find_pages_for_subject($subject["id"]); ?>
-					<ul class="pages">
-						<?php
-						while($page = mysqli_fetch_assoc($page_set)) {
-							?>
-							<li>
-								<a href="manage_content.php?page=<?php echo urlencode($page["id"]); ?>"><?php echo $page["menu_name"]; ?></a>
-							</li>
-							<?php
-						}
-						?>
-						<?php mysqli_free_result($page_set); ?>
-					</ul>
-				</li>
-				<?php
-			}
-			?>
-			<?php mysqli_free_result($subject_set); ?>
-		</ul>
+	<div id="main">
+		<div id="navigation">
+			<?php echo navigation($selected_subject_id, $selected_page_id); ?>
+		</div>
+		<div id="page">
+			<h2>Manage Content</h2>
+			<?php echo $selected_subject_id; ?><br />
+			<?php echo $selected_page_id; ?>
+		</div>
 	</div>
-	<div id="page">
-		<h2>Manage Content</h2>
-		<?php echo $selected_subject_id; ?><br />
-		<?php echo $selected_page_id; ?>
-	</div>
-</div>
 
 <?php include("../includes/layouts/footer.php"); ?>
