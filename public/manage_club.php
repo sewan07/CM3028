401f4f712342c60fab71e5178901ab0278aaa68c
Sewan <kido184@hotmail.com>
2016-12-09 07:39:41 +0000

    Index File

--- public/manage_club.php
+++ public/manage_club.php
@@ -1,29 +1,48 @@
 <?php require_once("../includes/db_connection.php"); ?>
 <?php require_once("../includes/functions.php"); ?>
-<?php
-// 2. Perform database query
-$query  = "SELECT * ";
-$query .= "FROM subjects ";
-$query .= "WHERE visible = 1 ";
-$query .= "AND id = 2 ";
-$query .= "ORDER BY position ASC";
-$result = mysqli_query($connection, $query);
-confirm_query($result);
-?>
 <?php include("../includes/layouts/header.php"); ?>
 
 <div id="main">
     <div id="navigation">
         <ul class="subjects">
             <?php
-            // 3. Use returned data (if any)
-            while($subject = mysqli_fetch_assoc($result)) {
-                // output data from each row
+            $query  = "SELECT * ";
+            $query .= "FROM subjects ";
+            $query .= "WHERE visible = 1 ";
+            $query .= "AND id = 2 ";
+            $query .= "ORDER BY position ASC";
+            $subject_set = mysqli_query($connection, $query);
+            confirm_query($subject_set);
+            ?>
+            <?php
+            while($subject = mysqli_fetch_assoc($subject_set)) {
                 ?>
-                <li><?php echo $subject["menu_name"]; ?></li>
+                <li>
+                    <?php echo $subject["menu_name"]; ?>
+                    <?php
+                    $query  = "SELECT * ";
+                    $query .= "FROM pages ";
+                    $query .= "WHERE visible = 1 ";
+                    $query .= "AND subject_id = 2 ";
+                    $query .= "ORDER BY position ASC";
+                    $page_set = mysqli_query($connection, $query);
+                    confirm_query($page_set);
+                    ?>
+                    <ul class="pages">
+                        <?php
+                        while($page = mysqli_fetch_assoc($page_set)) {
+                            ?>
+                            <li><?php echo $page["menu_name"]; ?></li>
+                            <?php
+                        }
+                        ?>
+                        <?php mysqli_free_result($page_set); ?>
+                    </ul>
+                </li>
                 <?php
             }
             ?>
+            <?php mysqli_free_result($subject_set); ?>
         </ul>
     </div>
     <div id="page">
@@ -31,10 +50,6 @@ confirm_query($result);
 
     </div>
 </div>
-<?php
-// 4. Release returned data
-mysqli_free_result($result);
-?>
 
 <?php include("../includes/layouts/footer.php"); ?>
 
