693be1fcf7aff0b4a57995418370826e273f8bcf
Sewan <kido184@hotmail.com>
2016-12-09 17:14:39 +0000

    Index File

--- includes/layouts/footer.php
+++ includes/layouts/footer.php
@@ -1,10 +1,4 @@
-    <div id="footer">Copyright <?php echo date("Y"); ?>, Coldpress Butter</div>
+    <div id="footer">Copyright 20xx, Widget Corp</div>
 
 	</body>
 </html>
-<?php
-  // 5. Close database connection
-	if (isset($connection)) {
-	  mysqli_close($connection);
-	}
-?>
--- includes/layouts/header.php
+++ includes/layouts/header.php
@@ -1,19 +1,12 @@
-<?php 
-	if (!isset($layout_context)) {
-		$layout_context = "public";
-	}
-?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
    "http://www.w3.org/TR/html4/loose.dtd">
 
 <html lang="en">
 	<head>
-		<title>Go Portlethen <?php if ($layout_context == "admin") { echo "Admin"; } ?></title>
+		<title>Widget Corp</title>
 		<link href="stylesheets/public.css" media="all" rel="stylesheet" type="text/css" />
 	</head>
 	<body>
     <div id="header">
-		<a href="index.php">  <h1>Go Portlethen <?php if ($layout_context == "admin") { echo "Admin"; } ?></h1> </a>
+      <h1>Widget Corp</h1>
     </div>
-	<div id="log">
-		<a href="choose_admin.php"> Log in </a> </div>
