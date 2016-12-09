05bdf5771c477970314992231299376557ae1dde
Sewan <kido184@hotmail.com>
2016-12-09 17:30:28 +0000

    Index File

--- public/manage_content.php
+++ public/manage_content.php
@@ -12,23 +12,23 @@ confirm_query($result);
 <?php include("../includes/layouts/header.php"); ?>
 
 <div id="main">
-  <div id="navigation">
-    <ul class="subjects">
-      <?php
-      // 3. Use returned data (if any)
-      while($subject = mysqli_fetch_assoc($result)) {
-        // output data from each row
-        ?>
-        <li><?php echo $subject["menu_name"] . " (" . $subject["id"] . ")"; ?></li>
-        <?php
-      }
-      ?>
-    </ul>
-  </div>
-  <div id="page">
-    <h2>Manage Content</h2>
+    <div id="navigation">
+        <ul class="subjects">
+            <?php
+            // 3. Use returned data (if any)
+            while($subject = mysqli_fetch_assoc($result)) {
+                // output data from each row
+                ?>
+                <li><?php echo $subject["menu_name"] . " (" . $subject["id"] . ")"; ?></li>
+                <?php
+            }
+            ?>
+        </ul>
+    </div>
+    <div id="page">
+        <h2>Manage Content</h2>
 
-  </div>
+    </div>
 </div>
 <?php
 // 4. Release returned data
