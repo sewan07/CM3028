510389568083958640ecc8f9a05be750aad4b8e2
Sewan <kido184@hotmail.com>
2016-12-09 08:12:07 +0000

    Index File

--- public/sportlethen.php
+++ public/sportlethen.php
@@ -32,7 +32,7 @@
                         <?php
                         while($page = mysqli_fetch_assoc($page_set)) {
                             ?>
-                            <li><?php echo $page["menu_name"]; ?></li>
+                   <a href="edit_club_page.php"> <li><?php echo $page["menu_name"]; ?></li> </a>
                             <?php
                         }
                         ?>
