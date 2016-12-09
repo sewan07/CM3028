df188d89084e7aad738a241a693fae5654645da9
Sewan <kido184@hotmail.com>
2016-12-09 18:41:28 +0000

    Index File

--- includes/functions.php
+++ includes/functions.php
@@ -19,12 +19,13 @@ function find_all_subjects() {
 	return $subject_set;
 }
 
-function find_pages_for_subject() {
+function find_pages_for_subject($subject_id) {
 	global $connection;
 
 	$query  = "SELECT * ";
 	$query .= "FROM pages ";
 	$query .= "WHERE visible = 1 ";
+	$query .= "AND subject_id = {$subject_id} ";
 	$query .= "AND id = 1 ";
 	$query .= "ORDER BY position ASC";
 	$page_set = mysqli_query($connection, $query);
