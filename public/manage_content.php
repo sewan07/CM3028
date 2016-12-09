67a6a6100417662827fd35632c03e2a535651c8d
Sewan <kido184@hotmail.com>
2016-12-09 19:29:12 +0000

    Index File

--- public/manage_content.php
+++ public/manage_content.php
@@ -3,27 +3,27 @@
 <?php include("../includes/layouts/header.php"); ?>
 
 <?php
-	if (isset($_GET["subject"])) {
-		$selected_subject_id = $_GET["subject"];
-		$selected_page_id = null;
-	} elseif (isset($_GET["page"])) {
-		$selected_subject_id = null;
-		$selected_page_id = $_GET["page"];
-	} else {
-		$selected_subject_id = null;
-		$selected_page_id = null;
-	}
+if (isset($_GET["subject"])) {
+	$selected_subject_id = $_GET["subject"];
+	$selected_page_id = null;
+} elseif (isset($_GET["page"])) {
+	$selected_subject_id = null;
+	$selected_page_id = $_GET["page"];
+} else {
+	$selected_subject_id = null;
+	$selected_page_id = null;
+}
 
 ?>
-<div id="main">
-  <div id="navigation">
-		<?php echo navigation($selected_subject_id, $selected_page_id); ?>
-  </div>
-  <div id="page">
-
-	    <h2>Manage Subject</h2>
-
-  </div>
-</div>
+	<div id="main">
+		<div id="navigation">
+			<?php echo navigation($selected_subject_id, $selected_page_id); ?>
+		</div>
+		<div id="page">
+			<h2>Manage Content</h2>
+			<?php echo $selected_subject_id; ?><br />
+			<?php echo $selected_page_id; ?>
+		</div>
+	</div>
 
 <?php include("../includes/layouts/footer.php"); ?>
