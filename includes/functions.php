c5c5dd9474927bef919d4c7fab37ddaf713c3fd5
Sewan <kido184@hotmail.com>
2016-12-09 18:28:38 +0000

    Index File

--- includes/functions.php
+++ includes/functions.php
@@ -1,37 +1,35 @@
 <?php
 
-	function confirm_query($result_set) {
-		if (!$result_set) {
-			die("Database query failed.");
-		}
+function confirm_query($result_set) {
+	if (!$result_set) {
+		die("Database query failed.");
 	}
+}
 
-	function find_all_subjects(){
-		// 2. Perform database query
-		global $connection;
+function find_all_subjects() {
+	global $connection;
 
-		$query  = "SELECT * ";
-		$query .= "FROM subjects ";
-		$query .= "WHERE visible = 1 ";
-		$query .= "AND id = 2 ";
-		$query .= "ORDER BY position ASC";
-		$subject_set = mysqli_query($connection, $query);
-		confirm_query($subject_set);
-		return $subject_set;
-	}
+	$query  = "SELECT * ";
+	$query .= "FROM subjects ";
+	$query .= "WHERE visible = 1 ";
+	$query .= "AND id = 2 ";
+	$query .= "ORDER BY position ASC";
+	$subject_set = mysqli_query($connection, $query);
+	confirm_query($subject_set);
+	return $subject_set;
+}
 
-	function find_club_pages(){
-		global $connection;
+function find_pages_for_subject($subject_id) {
+	global $connection;
 
-		// 2. Perform database query
-		$query  = "SELECT * ";
-		$query .= "FROM pages ";
-		$query .= "WHERE visible = 1 ";
-		$query .= "AND id = 1 ";
-		$query .= "ORDER BY position ASC";
-		$page_set = mysqli_query($connection, $query);
-		confirm_query($page_set);
-		return $page_set;
-	}
-	
+	$query  = "SELECT * ";
+	$query .= "FROM pages ";
+	$query .= "WHERE visible = 1 ";
+	$query .= "AND subject_id = {$subject_id} ";
+	$query .= "ORDER BY position ASC";
+	$page_set = mysqli_query($connection, $query);
+	confirm_query($page_set);
+	return $page_set;
+}
+
 ?>
--- public/manage_content.php
+++ public/manage_content.php
@@ -1,61 +1,38 @@
 <?php require_once("../includes/db_connection.php"); ?>
 <?php require_once("../includes/functions.php"); ?>
-
 <?php include("../includes/layouts/header.php"); ?>
 
 <div id="main">
-  <div id="navigation">
+	<div id="navigation">
 		<ul class="subjects">
+			<?php $subject_set = find_all_subjects(); ?>
 			<?php
-$subject_set = find_all_subjects();
-			?>
-		<?php
-			// 3. Use returned data (if any)
 			while($subject = mysqli_fetch_assoc($subject_set)) {
-				// output data from each row
-		?>
+				?>
 				<li>
 					<?php echo $subject["menu_name"]; ?>
-					<?php
-					$page_set = find_club_pages;
-					?>
-                <ul class=""pages">
-					<?php
-					// 3. Use returned data (if any)
-					while($page = mysqli_fetch_assoc($page_set)) {
-					// output data from each row
-					?>
-			<li>
-				<?php echo $page["menu_name"]; ?> </li>
-		<?php
-		}
-		?>
-			<?php
-			// 4. Release returned data
-			mysqli_free_result($page_set);
-			?>
-
-		</ul>
-
+					<?php $page_set = find_pages_for_subject($subject["id"]); ?>
+					<ul class="pages">
+						<?php
+						while($page = mysqli_fetch_assoc($page_set)) {
+							?>
+							<li><?php echo $page["menu_name"]; ?></li>
+							<?php
+						}
+						?>
+						<?php mysqli_free_result($page_set); ?>
+					</ul>
 				</li>
-	  <?php
+				<?php
 			}
-		?>
-	  <?php
-	  // 4. Release returned data
-	  mysqli_free_result($subject_set);
-	  ?>
+			?>
+			<?php mysqli_free_result($subject_set); ?>
+		</ul>
+	</div>
+	<div id="page">
+		<h2>Manage Content</h2>
 
-	  </ul>
-  </div>
-  <div id="page">
-    <h2>Manage Content</h2>
-
-  </div>
+	</div>
 </div>
-<?php
-  // 4. Release returned data
-  mysqli_free_result($result);
-?>
 
 <?php include("../includes/layouts/footer.php"); ?>
