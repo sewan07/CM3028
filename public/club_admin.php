4d32fb2bab487f9db0b149410c6ad67757f9c1b1
Sewan <kido184@hotmail.com>
2016-12-09 19:33:44 +0000

    Index File

---
+++ public/club_admin.php
@@ -0,0 +1,24 @@
+<?php require_once("../includes/session.php"); ?>
+<?php require_once("../includes/functions.php"); ?>
+<?php confirm_logged_in(); ?>
+
+<?php $layout_context = "admin"; ?>
+<?php include("../includes/layouts/header.php"); ?>
+
+<div id="main">
+    <div id="navigation">
+        &nbsp;
+    </div>
+    <div id="page">
+        <h2>Club Admin Menu</h2>
+        <p>Welcome to the club admin area, <?php echo htmlentities($_SESSION["username"]); ?>.</p>
+        <ul>
+            <li><a href="sportlethen.php">Sportlethen Sports</a></li>
+            <li><a href="huntly.php">Huntly Sports Hub</a></li>
+
+            <li><a href="logout.php">Logout</a></li>
+        </ul>
+    </div>
+</div>
+
+<?php include("../includes/layouts/footer.php"); ?>
