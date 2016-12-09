5ca51ce154c44ec9c0931c2298a4e79415a9b852
Sewan <kido184@hotmail.com>
2016-12-09 18:21:40 +0000

    Index File

--- includes/functions.php
+++ includes/functions.php
@@ -5,5 +5,33 @@
 			die("Database query failed.");
 		}
 	}
+
+	function find_all_subjects(){
+		// 2. Perform database query
+		global $connection;
+
+		$query  = "SELECT * ";
+		$query .= "FROM subjects ";
+		$query .= "WHERE visible = 1 ";
+		$query .= "AND id = 2 ";
+		$query .= "ORDER BY position ASC";
+		$subject_set = mysqli_query($connection, $query);
+		confirm_query($subject_set);
+		return $subject_set;
+	}
+
+	function find_club_pages(){
+		global $connection;
+
+		// 2. Perform database query
+		$query  = "SELECT * ";
+		$query .= "FROM pages ";
+		$query .= "WHERE visible = 1 ";
+		$query .= "AND id = 1 ";
+		$query .= "ORDER BY position ASC";
+		$page_set = mysqli_query($connection, $query);
+		confirm_query($page_set);
+		return $page_set;
+	}
 	
 ?>
