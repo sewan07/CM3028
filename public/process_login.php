73b9968a48b32c63e26f67403defeee758c73656
Sewan <kido184@hotmail.com>
2016-12-10 08:43:31 +0000

    Index File

--- public/process_login.php
+++ public/process_login.php
@@ -1,2 +1,10 @@
 <?php
-password_hash('secret', PASSWORD_DEFAULT);
+$password = "secret";
+$hash_format = "$2y$10$";
+$salt = "Salt22CharactersOrMore";
+echo "Length: " .strlen($salt);
+$format_and_salt = $hash_format . $salt;
+
+$hash = crypt($password, $format_and_salt);
+echo $hash;
+    ?>
