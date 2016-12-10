0c798da2038e5ca2f536f44f4f97d22dce9e39f1
Sewan <kido184@hotmail.com>
2016-12-10 13:12:10 +0000

    Index File

--- includes/header.php
+++ includes/header.php
@@ -1,4 +1,4 @@
-<?php require_once("../includes/db_connection.php"); ?>
+<?php require_once("db_connection.php"); ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
    "http://www.w3.org/TR/html4/loose.dtd">
 
