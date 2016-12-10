9afdcdca77f1546579912af287d1d051951b3617
Sewan <kido184@hotmail.com>
2016-12-10 17:22:23 +0000

    Index File

--- subjects.php
+++ subjects.php
@@ -1,13 +1,10 @@
 <?php
-// If categoryID is not set, redirect back to index page
-if(!isset($_GET['categoryID'])) {
-    header("Location:index.php");
-}
-// Select all stock items belonging to the selected categoryID
-$stock_sql="SELECT stock.stockID, stock.name, stock.price, category.name AS catname FROM stock JOIN category ON stock.categoryID=category.categoryID WHERE stock.categoryID=".$_GET['categoryID'];
-if($stock_query=mysqli_query($dbconnect, $stock_sql)) {
+$stock_sql="SELECT menu_name,content FROM pages JOIN subjects ON pages.subject_id=subjects.id WHERE pages.subject_id=2";
+
+if($stock_query=mysqli_query($dbconnect,$stock_sql)){
     $stock_rs=mysqli_fetch_assoc($stock_query);
 }
+
 if(mysqli_num_rows($stock_query)==0) {
     echo "Sorry, we have no items currently in stock";
 } else {
@@ -16,8 +13,8 @@ if(mysqli_num_rows($stock_query)==0) {
     <?php do {
         ?>
         <div class="item">
-            <p><?php echo $stock_rs['menu_name']; ?></p>
-            <p>$<?php echo $stock_rs['content']; ?></p>
+            <p><?php echo $stock_rs['name']; ?></p>
+            <p>$<?php echo $stock_rs['price']; ?></p>
         </div>
         <?php
     } while ($stock_rs=mysqli_fetch_assoc($stock_query));
