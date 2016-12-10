e793cb2f326121a90f76bf38d5887c1e835d24cc
Sewan <kido184@hotmail.com>
2016-12-10 17:06:51 +0000

    Index File

--- subjects.php
+++ subjects.php
@@ -1,5 +1,5 @@
 <?php
-$club_sql="SELECT menu_name,content FROM pages JOIN subjects ON pages.subject_id=subjects.id WHERE pages.subject_id=2"
-
+$club_sql="SELECT menu_name,content FROM pages JOIN subjects ON pages.subject_id=subjects.id WHERE pages.subject_id=2";
+echo $club_sql;
 
 ?>
