daf592c6bc3444df0f2ccf8b3221b7868e56c0a2
Sewan <kido184@hotmail.com>
2016-12-10 17:51:18 +0000

    Index File

--- subjects.php
+++ subjects.php
@@ -6,24 +6,5 @@ include("dbconnect.php");
 $stock_sql="SELECT menu_name,content FROM pages JOIN subjects ON pages.subject_id=subjects.id WHERE pages.subject_id=1";
 
 
-if($stock_query=mysqli_query($connection,$stock_sql)){
-    $stock_rs=mysqli_fetch_assoc($stock_query);
-}
 
-if(mysqli_num_rows($stock_query)==0) {
-    echo "Sorry, we have no items currently in stock";
-} else {
-    ?>
-    <h1><?php echo $stock_rs['catname']; ?></h1>
-    <?php do {
-        ?>
-        <div class="item">
-            <p><?php echo $stock_rs['menu_name']; ?></p>
-            <p>$<?php echo $stock_rs['content']; ?></p>
-        </div>
-        <?php
-    } while ($stock_rs=mysqli_fetch_assoc($stock_query));
-    ?>
-    <?php
-}
-?>
+
