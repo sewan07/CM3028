86dadf17e3dc99991d91e7f313507b97ad08aa6c
Sewan <kido184@hotmail.com>
2016-12-10 16:08:23 +0000

    Index File

--- header.php
+++ header.php
@@ -1,3 +1,7 @@
+<?php
+//This file is the base for all pages in the site. When creating a new page, we just open this one, then save a copy as the new page.
+include("dbconnect.php");
+?>
 <div class="header">
     	<div class="logo">
         <a href="index.php"><img src="images/logo.jpg" alt="Chic Clothing logo" /></a>
