d03862304aa25ee8050522c46f9cd675ce8ad775
Sewan <kido184@hotmail.com>
2016-12-10 19:14:43 +0000

    Index File

--- public/admin.php
+++ public/admin.php
@@ -1,17 +1,22 @@
-<?php require_once("../includes/session.php"); ?>
-<?php require_once("../includes/functions.php"); ?>
-<?php confirm_logged_in(); ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+    "http://www.w3.org/TR/html4/loose.dtd">
 
-<?php $layout_context = "admin"; ?>
-<?php include("../includes/layouts/header.php"); ?>
-
+<html lang="en">
+<head>
+  <title>untitled</title>
+  <link href="stylesheets/public.css" media="all" rel="stylesheet" type="text/css" />
+</head>
+<body>
+<div id="header">
+  <h1>Go Portlethen</h1>
+</div>
 <div id="main">
   <div id="navigation">
     &nbsp;
   </div>
   <div id="page">
     <h2>Admin Menu</h2>
-    <p>Welcome to the admin area, <?php echo htmlentities($_SESSION["username"]); ?>.</p>
+    <p>Welcome to the admin area.</p>
     <ul>
       <li><a href="manage_content.php">Manage Website Content</a></li>
       <li><a href="manage_admins.php">Manage Admin Users</a></li>
@@ -19,5 +24,8 @@
     </ul>
   </div>
 </div>
+<div id="footer">Copyright 20xx, Widget Corp</div>
 
-<?php include("../includes/layouts/footer.php"); ?>
+</body>
+</html>
+
