1d1c09d8bed0cd6ea7e5ed8e28ef7097d6afcdbc
Sewan <kido184@hotmail.com>
2016-12-10 17:52:03 +0000

    Index File

--- subjects.php
+++ subjects.php
@@ -1,9 +1,4 @@
-<?php
-//This file is the base for all pages in the site. When creating a new page, we just open this one, then save a copy as the new page.
-include("dbconnect.php");
-?>
-<?php
-$stock_sql="SELECT menu_name,content FROM pages JOIN subjects ON pages.subject_id=subjects.id WHERE pages.subject_id=1";
+<p>hello</p>
 
 
 
