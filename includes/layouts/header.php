c66895c120c06c49a10583a28c1494b35fc72b02
Sewan <kido184@hotmail.com>
2016-12-10 11:14:52 +0000

    Index File

--- includes/layouts/header.php
+++ includes/layouts/header.php
@@ -9,8 +9,9 @@
 <html lang="en">
 	<head>
 		<title>Go Portlethen <?php if ($layout_context == "admin") { echo "Admin"; } ?></title>
-		<link href="css/bootstrap.css" media="all" rel="stylesheet" type="text/css" />
+
 		<link href="stylesheets/public.css" media="all" rel="stylesheet" type="text/css" />
+		<link href="css/bootstrap.css" media="all" rel="stylesheet" type="text/css" />
 	</head>
 	<body>
     <div id="header">
