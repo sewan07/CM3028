5cf5eb02645ecaff04d6d147fac9cc89dfcdeb08
Sewan <kido184@hotmail.com>
2016-12-10 17:13:50 +0000

    Index File

--- subjects.php
+++ subjects.php
@@ -1,7 +1,7 @@
 <?php
-$club_sql="SELECT menu_name,content FROM pages JOIN subjects ON pages.subject_id=subjects.id WHERE pages.subject_id=2";
-echo $club_sql;
-if($stock_query=mysqli_query($dbconnect,$club_sql)){
+$stock_sql="SELECT menu_name,content FROM pages JOIN subjects ON pages.subject_id=subjects.id WHERE pages.subject_id=2";
+
+if($stock_query=mysqli_query($dbconnect,$stock_sql)){
     $stock_rs=mysqli_fetch_assoc($stock_query);
 }
 ?>
