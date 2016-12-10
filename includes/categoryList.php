f4e80481ee7785e7d1040f4ecbbd0b215b85bdc2
Sewan <kido184@hotmail.com>
2016-12-10 13:35:58 +0000

    Index File

---
+++ includes/categoryList.php
@@ -0,0 +1,13 @@
+<?php
+$cat_sql="SELECT * FROM subjects";
+$cat_query=mysqli_query($connection, $cat_sql);
+$cat_rs=mysqli_fetch_assoc($cat_query);
+
+do { ?>
+<a href="category.php?id=<?php echo $cat_rs['id']; ?>"><?php echo $cat_rs['menu_name']; ?></a>
+
+<?php
+
+} while ($cat_rs=mysqli_fetch_assoc($cat_query))
+
+    ?>
--- includes/header.php
+++ includes/header.php
@@ -12,18 +12,8 @@
 		<a href="index.php">  <h1>Go Portlethen</h1> </a>
     </div>
 	<div id="navigation">
-		<?php
-		$cat_sql="SELECT * FROM subjects";
-		$cat_query=mysqli_query($connection, $cat_sql);
-		$cat_rs=mysqli_fetch_assoc($cat_query);
-		?>
 		<p> <?php
-		do { ?>
-			<a href="category.php?id=<?php echo $cat_rs['id']; ?>"><?php echo $cat_rs['menu_name']; ?></a>
-
-				<?php
-
-		} while ($cat_rs=mysqli_fetch_assoc($cat_query))
+include("categoryList.php");
 
 ?>
 		<a href="admin.php">Admin</a>
