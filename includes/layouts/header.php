80a5e619d8dc7d2e71a8e5daf3aed89603e09200
Sewan <kido184@hotmail.com>
2016-12-11 15:43:27 +0000

    Index File

--- includes/layouts/header.php
+++ includes/layouts/header.php
@@ -16,7 +16,10 @@
 			integrity="sha256-iT6Q9iMJYuQiMWNd9lDyBUStIq/8PuOW33aOqmvFpqI="
 			crossorigin="anonymous"></script>
 
+		<script type="text/javascript" src="http://momentjs.com/downloads/moment-with-locales.min.js"></script>
 		<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.1.0/fullcalendar.min.js"></script>
+
+
 		<script type="text/javascript">
 			$(document).ready(function() {
 
