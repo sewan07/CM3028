8d418d0a0b878222301f44bb7d13dfe842ea9a36
Sewan <kido184@hotmail.com>
2016-12-11 18:13:43 +0000

    Index File

--- public/index.php
+++ public/index.php
@@ -24,7 +24,8 @@
 			 print_r($images);
 
             foreach($images as  $image) {
-                echo '<img src="http://visitportlethen.azurewebsites.net/public/images/'. $image . '">';
+                echo '<img src="img1.jpg">';
+                echo '<img src="img2.jpg">';
             }
 
 			 ?>
