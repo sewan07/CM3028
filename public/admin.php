dabc38099562cf2d30f0fb40570d9c880e7f23ac
Sewan <kido184@hotmail.com>
2016-12-11 16:21:28 +0000

    Index File

--- public/admin.php
+++ public/admin.php
@@ -13,9 +13,9 @@
     <h2>Admin Menu</h2>
     <p>Welcome to the admin area, <?php echo htmlentities($_SESSION["username"]); echo '>>>' . $_SESSION["username"]; ?>.</p>
     <ul>
-      <?php if($_SESSION["username"] == "umesh"):?>
+      <?php if($_SESSION["username"] == "umesh"){?>
       <li><a href="manage_content.php">Manage Website Content</a></li>
-      <?php endif;?>
+      <?php }?>
       <li><a href="manage_admins.php">Manage Admin Users</a></li>
       <li><a href="logout.php">Logout</a></li>
     </ul>
