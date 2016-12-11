e5441d86e5fcc4445969aea334196c5566a7efd4
Sewan <kido184@hotmail.com>
2016-12-11 07:19:49 +0000

    Index File

--- public/manage_club_page.php
+++ public/manage_club_page.php
@@ -1,7 +1,6 @@
 <?php require_once("../includes/db_connection.php"); ?>
 <?php require_once("../includes/function_club.php"); ?>
 <?php include("../includes/layouts/header.php"); ?>
-
 <?php
 if (isset($_GET["subject"])) {
     $selected_subject_id = $_GET["subject"];
@@ -15,15 +14,42 @@ if (isset($_GET["subject"])) {
 }
 
 ?>
-<div id="main">
-    <div id="navigation">
-        <?php echo navigation($selected_subject_id, $selected_page_id); ?>
+    <div id="main">
+        <div id="navigation">
+            <ul class="subjects">
+                <?php
+                $subject_set = find_all_subjects() ?>
+                ?>
+                <?php
+                while($subject = mysqli_fetch_assoc($subject_set)) {
+                    ?>
+                    <li>
+                        <a href="manage_club_page.php?subject=<?php echo urlencode($subject["id"]); ?>"><?php echo $subject["menu_name"]; ?> </a>
+                        <?php $page_set =  find_all_pages_for_subject($subject["id"]);?>
+                        <ul class="pages">
+                            <?php
+                            while($page = mysqli_fetch_assoc($page_set)) {
+                                ?>
+                                <li>
+                                    <a href="manage_club_page.php?page=<?php echo urlencode($page["id"]); ?>"> <?php echo $page["menu_name"]; ?> </a>
+                                </li>
+                                <?php
+                            }
+                            ?>
+                            <?php mysqli_free_result($page_set); ?>
+                        </ul>
+                    </li>
+                    <?php
+                }
+                ?>
+                <?php mysqli_free_result($subject_set); ?>
+            </ul>
+        </div>
+        <div id="page">
+            <h2>Manage Content</h2>
+            <?php echo $selected_subject_id; ?><br />
+            <?php echo $selected_page_id; ?>
+        </div>
     </div>
-    <div id="page">
-        <h2>Manage Content</h2>
-        <?php echo $selected_subject_id; ?><br />
-        <?php echo $selected_page_id; ?>
-    </div>
-</div>
 
 <?php include("../includes/layouts/footer.php"); ?>
