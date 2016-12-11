00eb45e84d019ac9c2e8f346d3e4f8d965311994
Sewan <kido184@hotmail.com>
2016-12-11 12:08:18 +0000

    Index File

--- public/new_sportlethen_page.php
+++ public/new_sportlethen_page.php
@@ -1,6 +1,6 @@
 <?php require_once("../includes/session.php"); ?>
 <?php require_once("../includes/db_connection.php"); ?>
-<?php require_once("../includes/function_club.php"); ?>
+<?php require_once("../includes/functions.php"); ?>
 <?php require_once("../includes/validation_functions.php"); ?>
 
 <?php find_selected_page(); ?>
