ea31178c14464f3ddea10da38dabf7fedfcf55f5
Sewan <kido184@hotmail.com>
2016-12-11 07:39:46 +0000

    Index File

--- includes/function_club.php
+++ includes/function_club.php
@@ -5,22 +5,24 @@ function confirm_query($result_set) {
         die("Database query failed.");
     }
 }
-function find_all_subjects(){
+
+function find_all_subjects() {
     global $connection;
+
     $query  = "SELECT * ";
     $query .= "FROM subjects ";
-    $query .= "WHERE visible = 1 ";
-    $query .= "AND id = 2 ";
+    // $query .= "WHERE visible = 1 ";
     $query .= "ORDER BY position ASC";
     $subject_set = mysqli_query($connection, $query);
     confirm_query($subject_set);
     return $subject_set;
 }
 
-function find_all_pages_for_subject($subject_id){
+function find_pages_for_subject($subject_id) {
     global $connection;
 
     $safe_subject_id = mysqli_real_escape_string($connection, $subject_id);
+
     $query  = "SELECT * ";
     $query .= "FROM pages ";
     $query .= "WHERE visible = 1 ";
@@ -31,46 +33,41 @@ function find_all_pages_for_subject($subject_id){
     return $page_set;
 }
 
-function find_subject_by_id($subject_id){
+function find_subject_by_id($subject_id) {
     global $connection;
 
     $safe_subject_id = mysqli_real_escape_string($connection, $subject_id);
+
     $query  = "SELECT * ";
     $query .= "FROM subjects ";
-    $query .= "AND id = (subject_id) ";
+    $query .= "WHERE id = {$safe_subject_id} ";
     $query .= "LIMIT 1";
     $subject_set = mysqli_query($connection, $query);
     confirm_query($subject_set);
-   if( $subject = mysqli_fetch_assoc($subject_set)){
+    if($subject = mysqli_fetch_assoc($subject_set)) {
+        return $subject;
+    } else {
+        return null;
+    }
+}
 
-    return $subject;
-   } else {
-   return null; }}
+function find_page_by_id($page_id) {
+    global $connection;
 
+    $safe_page_id = mysqli_real_escape_string($connection, $page_id);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    $query  = "SELECT * ";
+    $query .= "FROM pages ";
+    $query .= "WHERE id = {$safe_page_id} ";
+    $query .= "LIMIT 1";
+    $page_set = mysqli_query($connection, $query);
+    confirm_query($page_set);
+    if($page = mysqli_fetch_assoc($page_set)) {
+        return $page;
+    } else {
+        return null;
+    }
+}
 
 // navigation takes 2 arguments
 // - the currently selected subject ID (if any)
@@ -84,7 +81,7 @@ function navigation($subject_id, $page_id) {
             $output .= " class=\"selected\"";
         }
         $output .= ">";
-        $output .= "<a href=\"manage_club_page.php?subject=";
+        $output .= "<a href=\"manage_content.php?subject=";
         $output .= urlencode($subject["id"]);
         $output .= "\">";
         $output .= $subject["menu_name"];
@@ -98,7 +95,7 @@ function navigation($subject_id, $page_id) {
                 $output .= " class=\"selected\"";
             }
             $output .= ">";
-            $output .= "<a href=\"manage_club_page.php?page=";
+            $output .= "<a href=\"manage_content.php?page=";
             $output .= urlencode($page["id"]);
             $output .= "\">";
             $output .= $page["menu_name"];
@@ -113,8 +110,6 @@ function navigation($subject_id, $page_id) {
 }
 
 ?>
-
-
 
 
 
--- public/manage_club_page.php
+++ public/manage_club_page.php
@@ -1,6 +1,7 @@
 <?php require_once("../includes/db_connection.php"); ?>
-<?php require_once("../includes/function_club.php"); ?>
+<?php require_once("../includes/functions.php"); ?>
 <?php include("../includes/layouts/header.php"); ?>
+
 <?php
 if (isset($_GET["subject"])) {
     $selected_subject_id = $_GET["subject"];
@@ -14,53 +15,25 @@ if (isset($_GET["subject"])) {
 }
 
 ?>
-    <div id="main">
-        <div id="navigation">
-            <ul class="subjects">
-                <?php
-                $subject_set = find_all_subjects() ?>
-                ?>
-                <?php
-                while($subject = mysqli_fetch_assoc($subject_set)) {
-                    ?>
-                    <li>
-                        <a href="manage_club_page.php?subject=<?php echo urlencode($subject["id"]); ?>"><?php echo $subject["menu_name"]; ?> </a>
-                        <?php $page_set =  find_all_pages_for_subject($subject["id"]);?>
-                        <ul class="pages">
-                            <?php
-                            while($page = mysqli_fetch_assoc($page_set)) {
-                                ?>
-                                <li>
-                                    <a href="manage_club_page.php?page=<?php echo urlencode($page["id"]); ?>"> <?php echo $page["menu_name"]; ?> </a>
-                                </li>
-                                <?php
-                            }
-                            ?>
-                            <?php mysqli_free_result($page_set); ?>
-                        </ul>
-                    </li>
-                    <?php
-                }
-                ?>
-                <?php mysqli_free_result($subject_set); ?>
-            </ul>
-        </div>
-        <div id="page">
-
-            <?php if ($selected_subject_id) { ?>
-                <h2>Manage Subject</h2>
-                <?php $current_subject = find_subject_by_id($selected_subject_id); ?>
-                Menu name: <?php echo $current_subject["menu_name"]; ?><br />
-
-            <?php } elseif ($selected_page_id) { ?>
-                <h2>Manage Page</h2>
-                <?php $current_page = find_page_by_id($selected_page_id); ?>
-                Menu name: <?php echo $current_page["menu_name"]; ?><br />
-
-            <?php } else { ?>
-                Please select a page.
-            <?php }?>
-        </div>
+<div id="main">
+    <div id="navigation">
+        <?php echo navigation($selected_subject_id, $selected_page_id); ?>
     </div>
+    <div id="page">
+        <?php if ($selected_subject_id) { ?>
+            <h2>Manage Subject</h2>
+            <?php $current_subject = find_subject_by_id($selected_subject_id); ?>
+            Menu name: <?php echo $current_subject["menu_name"]; ?><br />
+
+        <?php } elseif ($selected_page_id) { ?>
+            <h2>Manage Page</h2>
+            <?php $current_page = find_page_by_id($selected_page_id); ?>
+            Menu name: <?php echo $current_page["menu_name"]; ?><br />
+
+        <?php } else { ?>
+            Please select a subject or a page.
+        <?php }?>
+    </div>
+</div>
 
 <?php include("../includes/layouts/footer.php"); ?>
