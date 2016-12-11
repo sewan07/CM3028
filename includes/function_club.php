50c3f6f13f315368daf1a621b61c6a710e9323ed
Sewan <kido184@hotmail.com>
2016-12-11 08:12:22 +0000

    Index File

--- includes/function_club.php
+++ includes/function_club.php
@@ -70,19 +70,35 @@ function find_page_by_id($page_id) {
     }
 }
 
+function find_selected_page() {
+    global $current_subject;
+    global $current_page;
+
+    if (isset($_GET["subject"])) {
+        $current_subject = find_subject_by_id($_GET["subject"]);
+        $current_page = null;
+    } elseif (isset($_GET["page"])) {
+        $current_subject = null;
+        $current_page = find_page_by_id($_GET["page"]);
+    } else {
+        $current_subject = null;
+        $current_page = null;
+    }
+}
+
 // navigation takes 2 arguments
-// - the currently selected subject ID (if any)
-// - the currently selected page ID (if any)
-function navigation($subject_id, $page_id) {
+// - the current subject array or null
+// - the current page array or null
+function navigation($subject_array, $page_array) {
     $output = "<ul class=\"subjects\">";
     $subject_set = find_all_subjects();
     while($subject = mysqli_fetch_assoc($subject_set)) {
         $output .= "<li";
-        if ($subject["id"] == $subject_id) {
+        if ($subject_array && $subject["id"] == $subject_array["id"]) {
             $output .= " class=\"selected\"";
         }
         $output .= ">";
-        $output .= "<a href=\"manage_club_page.php?subject=";
+        $output .= "<a href=\"manage_content.php?subject=";
         $output .= urlencode($subject["id"]);
         $output .= "\">";
         $output .= $subject["menu_name"];
@@ -92,11 +108,11 @@ function navigation($subject_id, $page_id) {
         $output .= "<ul class=\"pages\">";
         while($page = mysqli_fetch_assoc($page_set)) {
             $output .= "<li";
-            if ($page["id"] == $page_id) {
+            if ($page_array && $page["id"] == $page_array["id"]) {
                 $output .= " class=\"selected\"";
             }
             $output .= ">";
-            $output .= "<a href=\"manage_club_page.php?page=";
+            $output .= "<a href=\"manage_content.php?page=";
             $output .= urlencode($page["id"]);
             $output .= "\">";
             $output .= $page["menu_name"];
@@ -111,8 +127,3 @@ function navigation($subject_id, $page_id) {
 }
 
 ?>
-
-
-
-
-
--- public/manage_club_page.php
+++ public/manage_club_page.php
@@ -1,33 +1,19 @@
 <?php require_once("../includes/db_connection.php"); ?>
-<?php require_once("../includes/function_club.php"); ?>
+<?php require_once("../includes/functions.php"); ?>
 <?php include("../includes/layouts/header.php"); ?>
+<?php find_selected_page(); ?>
 
-<?php
-if (isset($_GET["subject"])) {
-    $selected_subject_id = $_GET["subject"];
-    $selected_page_id = null;
-} elseif (isset($_GET["page"])) {
-    $selected_subject_id = null;
-    $selected_page_id = $_GET["page"];
-} else {
-    $selected_subject_id = null;
-    $selected_page_id = null;
-}
-
-?>
 <div id="main">
     <div id="navigation">
-        <?php echo navigation($selected_subject_id, $selected_page_id); ?>
+        <?php echo navigation($current_subject, $current_page); ?>
     </div>
     <div id="page">
-        <?php if ($selected_subject_id) { ?>
+        <?php if ($current_subject) { ?>
             <h2>Manage Subject</h2>
-            <?php $current_subject = find_subject_by_id($selected_subject_id); ?>
             Menu name: <?php echo $current_subject["menu_name"]; ?><br />
 
-        <?php } elseif ($selected_page_id) { ?>
+        <?php } elseif ($current_page) { ?>
             <h2>Manage Page</h2>
-            <?php $current_page = find_page_by_id($selected_page_id); ?>
             Menu name: <?php echo $current_page["menu_name"]; ?><br />
 
         <?php } else { ?>
